c396a7eae44469dd718946d2146513b3d1d23932
Ian Christie <iankchristie@gmail.com>
2024-10-19 06:59:17 -0600

    Add macrostrat downloader.

---
+++ macrostrat.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://macrostrat.org/api/v2/mobile/map_query_v2"
+
+# z is zoom, testing on the map shows that 12 seems sufficient.
+params = {"lng": -100.402, "lat": 33.424, "z": 12}
+
+headers = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
