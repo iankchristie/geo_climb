4e9e4a7578b32f7fa6391257ab1ac2285542219d
Ankur Varma <ankur.varma@live.com>
2024-11-23 17:49:40 -0700

    Some changes to flattening

--- encoders/flatten.py
+++ encoders/flatten.py
@@ -13,21 +13,28 @@ def flatten_N_save(type: Literal["sen", "dem"], data: str, dir: str):
     Flatten and save the given data.
 
     Args:
-        type (str): Type of the data, either "SEN" or "DEM".
+        type (str): Type of the data, either "sen" or "dem".
         data (str): Path or reference to the data to be processed.
         dir (str): Directory where the flattened data should be saved.
 
     """
     for file in os.listdir(data):
-        output_dir=os.path.join(dir,f"{data}_flattened")
+        output_dir=os.path.join(dir,f"{type}_flattened")
         os.makedirs(output_dir, exist_ok=True)
         if file.endswith(".tif"):
             lat, lon = decode_file(file)
-            with rasterio.open(os.path.join(data, file)) as src:
-                image = src.read()  # This returns a 3D array (bands, height, width)
-            if  type=="DEM":    
+            if  type=="dem":   
+                with rasterio.open(os.path.join(data, file)) as src:
+                    image = src.read(1) 
                 image_tensor = preprocess_dem(image)
             else:
+                with rasterio.open(os.path.join(data, file)) as src:
+                    blue = src.read(1)  # Band 1 (B2: Blue)
+                    green = src.read(2)  # Band 2 (B3: Green)
+                    red = src.read(3)  # Band 3 (B4: Red)
+
+                # Stack the bands into a 3D array (R, G, B)
+                image = np.dstack((red, green, blue))
                 image_tensor=preprocess_sen(image)
             flattened_image = image_tensor.numpy().flatten()
             print(flattened_image.shape)
